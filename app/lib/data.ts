0dfd689c9862059b411fe0eed45398d465e523ac
zunaizah <zunaizah314@gmail.com>
2025-12-09 16:45:33 +0700

    sampe chap 8

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,16 +14,15 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
+    // We artificially delay a response for demo purposes.
     // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+ 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
+ 
+    console.log('Data fetch completed after 3 seconds.');
+ 
     return data;
   } catch (error) {
     console.error('Database Error:', error);
@@ -62,6 +61,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
+    
 
     const data = await Promise.all([
       invoiceCountPromise,
