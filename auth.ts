bee206632fe015d621690ecade7279a4489bc04a
zunaizah <zunaizah314@gmail.com>
2025-12-21 18:03:51 +0700

    switch ke bcryptjs

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import postgres from 'postgres';
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
